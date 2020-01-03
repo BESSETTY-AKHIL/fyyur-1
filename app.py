1fb7ff1bebe37b0c4e301c8de1ac543f54c004ce
Hsiao Li Lin <hsiaolilin@Hsiaos-MacBook-Air.local>
2020-01-03 17:44:47 -0500

    edit @app.route('/artists/create', methods=['POST'])

--- app.py
+++ app.py
@@ -500,13 +500,27 @@ def create_artist_submission():
   # called upon submitting the new artist listing form
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
-
+  try:
+    new_artist = Artist(**{
+      "name":request.form['name'],
+      "city": request.form['city'],
+      "state": request.form['state'],
+      "phone":  request.form['phone'],
+      "genres": request.form['genres'],
+      "facebook_link": request.form['facebook_link'],
+    })
+    db.session.add(new_artist)
+    db.session.commit()
   # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
+    flash('Artist ' + request.form['name'] + ' was successfully listed!')
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+  except:
+       db.session.rollback()
+       flash('An error occurred. Venue ' + request.form['name'] + ' could not be listed.')
+  finally:
+       db.session.close()
   return render_template('pages/home.html')
-
 
 #  Shows
 #  ----------------------------------------------------------------
