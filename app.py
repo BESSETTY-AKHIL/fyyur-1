8d5b628248c75dd805b34b1c8ee04eb3fc0d3a1a
Hsiao Li Lin <hsiaolilin@Hsiaos-MacBook-Air.local>
2020-01-03 10:09:12 -0500

    add Model 'Show'

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
+    name = db.Column(db.String(), unique=True, nullable=False)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
@@ -47,11 +47,14 @@ class Venue(db.Model):
     seeking_description = db.Column(db.String(120),nullable=True )
     shows = db.relationship('Show', backref='Venue',lazy=True)
 
+    def __repr__(self):
+        return f'<{self.id} {self.name}>'
+
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
+    name = db.Column(db.String(), unique=True, nullable=False)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
@@ -61,11 +64,25 @@ class Artist(db.Model):
 
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
     website = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120),default=False)
     shows = db.relationship('Show', backref='Artist', lazy=True)
+
+    def __repr__(self):
+        return f'<{self.id} {self.name}>'
+
+    # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'show'
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey(Venue.id), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey(Artist.id), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f'<artist_id: {self.artist_id} venue_id: {self.venue_id}>'
+
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
