d20ee408f0e7e670a2a43d01669820618e688835
Hsiao Li Lin <hsiaolilin@Hsiaos-MacBook-Air.local>
2020-01-04 00:06:32 -0500

    @app.route('/artists/search')

--- app.py
+++ app.py
@@ -356,15 +356,22 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
+  search_term = request.form['search_term'].lower()
+  artists_query = Artist.query.filter(func.lower(Artist.name).like('%{}%'.format(search_term.lower()))
+  ).all()
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": len(artists_query),
+    "data": []
   }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+  for artist in artists_query:
+      response["data"].append(
+           {
+                "id": artist.id,
+                "name": artist.name,
+                "num_upcoming_shows": artist.upcoming_shows,
+            }
+      )
+  return render_template('pages/search_artists.html', results=response, search_term=search_term)
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
